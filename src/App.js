4c2d8977184386b34de823c44b0270c0cbf5b38f
unknown <hasan.rajib1996@gmail.com>
2021-12-09 00:15:37 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Rajib Hasan</h1>
+    <div>
+      <Home />
     </div>
   );
 }
