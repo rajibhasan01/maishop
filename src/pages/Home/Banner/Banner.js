5a044732ab477638ff7f2a45ed1edd47667dd660
unknown <hasan.rajib1996@gmail.com>
2021-12-13 10:34:08 +0600

    updated

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -13,21 +13,19 @@ const Banner = () => {
                     <div className="col-12 col-md-3">
                         <div className="row g-2">
                             <div className="col-6 col-md-12 pb-md-1">
-                                <div className='w-100'>
+                                <div className='w-100 imgHover'>
                                     <img src="https://i.ibb.co/xsRHxFj/18899269.jpg" alt="" className='img-fluid' />
                                 </div>
                             </div>
                             <div className="col-6 col-md-12">
-                                <div className='w-100'>
+                                <div className='w-100 imgHover'>
                                     <img src="https://i.ibb.co/GJdBmVQ/18899255.jpg" alt="" className='img-fluid' />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-            </div >
-
+            </div>
         </div>
 
     );
