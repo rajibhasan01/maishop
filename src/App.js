043ca33f79ab1f5fd2f378593bbc074e0aa17a0f
rajibhasan01 <hasan.rajib1996@gmail.com>
2024-01-11 14:14:20 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home/Home/Home';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home/Home/Home";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-
           <Route exact path="/">
             <Home />
           </Route>
-
         </Switch>
       </Router>
-
     </div>
   );
 }
