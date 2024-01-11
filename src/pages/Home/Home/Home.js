0b4b06b7d6f1ba3618a07a5872276a135bd30f3c
rajibhasan01 <hasan.rajib1996@gmail.com>
2024-01-11 16:11:53 +0600

    ADD: Footer added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../../Header/Header/Header';
 import Banner from '../Banner/Banner';
+import Footer from '../../../components/Shared/Footer/Footer';
+
 
 const Home = () => {
     return (
         <div>
             <Header />
             <Banner />
+            <Footer />
         </div>
     );
 };
